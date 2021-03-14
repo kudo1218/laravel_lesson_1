e55e982718deaabc653681c7a189543c52c51ebf
fromlabo <fromlabo@fromlabonoMacBook-Air.local>
2021-03-14 20:03:48 +0900

    laravel課題1/完成

---
+++ lesson_one/resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>laravel課題1</title>
+</head>
+<body>
+  <form action="login" method="get">
+    <button>ログイン画面へ移動</button>
+  </form>
+  <form action="register" method="get">
+    <button>登録画面へ移動</button><br>
+  </form>
+
+  <h2>ログイン画面</h2>
+  E-Mail Address<input type="text"><br>
+  Password<input type="text"><br>
+  <input type="submit" value="ログイン">
+</body>
+</html>
---
+++ lesson_one/resources/views/register.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>laravel課題1</title>
+</head>
+<body>
+  <form action="login" method="get">
+    <button>ログイン画面へ移動</button>
+  </form>
+  <form action="register" method="get">
+    <button>登録画面へ移動</button><br>
+  </form>
+
+  <h2>登録画面</h2>
+  Name<input type="text"><br>
+  E-Mail Address<input type="text"><br>
+  Password<input type="text"><br>
+  Confirm Passwprd<input type="text"><br>
+  <input type="submit" value="登録">
+</body>
+</html>
