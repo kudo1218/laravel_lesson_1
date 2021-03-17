07fcb4b188705c510d2c55f06b73b19146aae7de
fromlabo <fromlabo@fromlabonoMacBook-Air.local>
2021-03-17 21:36:07 +0900

    laravel課題2/途中

--- lesson_one/resources/views/register.blade.php
+++ lesson_one/resources/views/register.blade.php
@@ -4,7 +4,7 @@
   <h2>新規登録ページ</h2>
   <form action="/home" method="get">
     <input type="text" name="name" placeholder="Name"><br>
-    <input type="text" name="mail" placeholder="E-Mail Address"><br>
+    <input type="text" name="mail" placeholder="Mail Address"><br>
     <input type="password" name="password" placeholder="Password"><br>
     <input type="password" name="password_confirmation" placeholder="Confirm Password"><br>
     <input type="submit" value="登録">
